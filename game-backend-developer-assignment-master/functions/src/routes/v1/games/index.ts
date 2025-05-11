825da8f4c2314b1301ca781f0d4c0a576dc1e9a7
Alp Kurt <krtalp@gmail.com>
2025-05-11 12:50:01 +0200

    feat(api): add POST, PUT, DELETE endpoints to /api/v1/games

--- game-backend-developer-assignment-master/functions/src/routes/v1/games/index.ts
+++ game-backend-developer-assignment-master/functions/src/routes/v1/games/index.ts
@@ -1,9 +1,54 @@
-import { wrapAsync, createRouter } from '../../../utils/index.js';
-import { getGames } from '../../../apis/firestore/games.js';
+import { Router } from 'express';
+import { getGames } from '../../../apis/firestore/games';
+import { getFirestore } from '../../../apis/firestore/getFirestore';
+import { HttpError } from '../../../classes/HttpError';
 
-export const gamesRouter = createRouter();
+const router = Router();
 
-gamesRouter.get(
-  '/',
-  wrapAsync(() => getGames()),
-);
+// GET all games
+router.get('/', async (_req, res) => {
+  const games = await getGames();
+  res.status(200).json(games);
+});
+
+// POST /api/v1/games
+router.post('/', async (req, res, next) => {
+  try {
+    const game = req.body;
+    if (!game?.id) throw new HttpError('Game ID is required', 400);
+
+    await getFirestore().collection('games').doc(game.id).set(game);
+    res.status(201).json({ message: 'Game created', id: game.id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PUT /api/v1/games/:id
+router.put('/:id', async (req, res, next) => {
+  try {
+    const game = req.body;
+    const { id } = req.params;
+    if (!id) throw new HttpError('Game ID is required', 400);
+
+    await getFirestore().collection('games').doc(id).update(game);
+    res.status(200).json({ message: 'Game updated', id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE /api/v1/games/:id
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!id) throw new HttpError('Game ID is required', 400);
+
+    await getFirestore().collection('games').doc(id).delete();
+    res.status(200).json({ message: 'Game deleted', id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
